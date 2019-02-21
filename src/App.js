186ef5bf86ee7ee7e6b58e47927cb1aff41bf6a7
Anastasia Malissovas <anastasiamalissovas@gmail.com>
2019-02-21 10:00:14 -0500

    Started by writing some directi writing directions for what I will code.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+//import { Http2ServerRequest } from 'http2'
+const url = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2/'
+// save url as a string to you can concatinate with each path (already done)
+// save json to file in your app (make new file in your app)
+// import json file (add import to top of file)
+// call an enumerable (like Array.map, see previous homework example below)
+// map over each of the values in the array of movie objects.
+// (like below, but each arg will be an object, so use . notation to access attributes)
+// ex movies.map(movie => <MovieComponent name={movie.name} src={url + movie.poster_path}/>)
 
+// sample of mapping over cats array
+// {cats.map(catImages => (
+//   <Octocats catImages={catImages} />
+// ))}
+
+// sample of import
+// import Response from ./movies.json
 class App extends Component {
   render() {
-    return <HelloWorld />
+    return (
+      <>
+        <header />
+        <section>
+          {' '}
+          <img src={url + '4p1N2Qrt8j0H8xMHMHvtRxv9weZ.jpg'} />
+          <img src={url + 'kBf3g9crrADGMc2AMAMlLBgSm2h.jpg'} />
+          <img src={url + '3Ri2GReavqSHqWemlP6HYn8i2P9.jpg'} />
+        </section>
+      </>
+    )
   }
 }
 
